4999f419b512a304e454083e194fdf78bd16b06f
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-10-19 00:39:05 +0900

    login

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./Login";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div style={{ fontFamily: "Avenir" }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
