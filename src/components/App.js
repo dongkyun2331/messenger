f4c630712c8d5339bc689f99fb63633d4d64be86
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-03 01:42:28 +0900

    chats

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,21 @@ import React from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import { AuthProvider } from "../contexts/AuthContext";
+
+import Chats from "./Chats";
 import Login from "./Login";
 
 const App = () => {
   return (
     <div style={{ fontFamily: "Avenir" }}>
       <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/chats" element={<Chats />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </AuthProvider>
       </Router>
     </div>
   );
