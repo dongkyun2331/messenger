f6878fb786186024ee8e9e29621ec959da4d229b
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-04 00:12:12 +0900

    chat engine

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "../firebase";
 
 const Chats = () => {
-  return <h1>Chats</h1>;
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await auth.signOut();
+
+    navigate("/");
+  };
+
+  return (
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">Porichat</div>
+        <div onClick={handleLogout} className="logout-tab">
+          Logout
+        </div>
+      </div>
+
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectId={process.env.REACT_APP_CHAT_ENGINE_ID}
+        userName=""
+        userSecret=""
+      />
+    </div>
+  );
 };
 
 export default Chats;
